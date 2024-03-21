0fd9085b2daea9d2b16ab3a41317253edeb0ab52
Cody Thornell <cody@Codys-Laptop.local>
2024-03-21 10:10:33 -0400

    setup sts file

---
+++ contactsystem/src/main/java/com/example/contactsystem/ContactsystemApplication.java
@@ -0,0 +1,13 @@
+package com.example.contactsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ContactsystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ContactsystemApplication.class, args);
+	}
+
+}
---
+++ contactsystem/src/test/java/com/example/contactsystem/ContactsystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.contactsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ContactsystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
